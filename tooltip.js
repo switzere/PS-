f7187a2b17a5cebe07cdbba4303ea9ea1b7fdabc
switzere <switzere@uoguelph.ca>
2024-09-30 17:59:50 -0400

    added in move damage (should probably make the typeEffectivenessChart its own file)

--- tooltip.js
+++ tooltip.js
@@ -27,6 +27,44 @@ function calculateStats(pokemon, level) {
   return stats;
 }
 
+function calculateDamage(move, activeStats, foeStats, activePokemonBaseSpecies, foePokemonBaseSpecies, level) {
+  //gets the damage of the move, then using the base power, attacker's attack, defender's defense, and the level, calculates the damage
+  if (move.basePower === 0) return [0, 0];
+
+  //calculate the damage, we have the stats, the base power, level, and type
+  let attacker = 0;
+  let defender = 0;
+  if(move.category === 'Special') {
+    attacker = activeStats.spa;
+    defender = foeStats.spd;
+  } else if(move.category === 'Physical') {
+    attacker = activeStats.atk;
+    defender = foeStats.def;
+  }
+
+  let activeType1 = activePokemonBaseSpecies.types[0];
+  let activeType2 = activePokemonBaseSpecies.types[1];
+
+  let foeType1 = foePokemonBaseSpecies.types[0];
+  let foeType2 = foePokemonBaseSpecies.types[1];
+
+  // Use typeEffectivenessChart to calculate type effectiveness
+  let typeEffectiveness = typeEffectivenessChart[move.type][foeType1] * (foeType2 ? typeEffectivenessChart[move.type][foeType2] : 1);
+
+  let stab = move.type === activeType1 || move.type === activeType2 ? 1.5 : 1;
+
+  // damage floor
+  let damageFloor = ((((2 * level)/5 + 2) * move.basePower * (attacker/defender)) / 50 + 2) * 0.85 * typeEffectiveness * stab;
+
+  // damage ceiling
+  let damageCeiling = ((((2 * level)/5 + 2) * move.basePower * (attacker/defender)) / 50 + 2) * 1 * typeEffectiveness * stab;
+
+  return [damageFloor, damageCeiling]; // Return damage range as an array
+
+
+
+}
+
 ShowdownEnhancedTooltip.showPokemonTooltip = function showPokemonTooltip(clientPokemon, serverPokemon, isActive, illusionIndex) {
   // Call the original method
   let text = originalShowPokemonTooltip.call(this, clientPokemon, serverPokemon, isActive, illusionIndex);
@@ -34,12 +72,6 @@ ShowdownEnhancedTooltip.showPokemonTooltip = function showPokemonTooltip(clientP
   //console.log(originalShowPokemonTooltip);
 
   text += '<hr style="border: 1px solid black; margin: 5px 0;">';
-
-  if (!serverPokemon) {
-    if (!clientPokemon) throw new Error('Must pass either clientPokemon or serverPokemon');
-    let [, max] = this.getSpeedRange(clientPokemon);
-    text += '<p><small>Spe</small> ' + max + ' <small>(before items/abilities/modifiers)</small></p>';
-  }
 
   if (!serverPokemon) {
     if (!clientPokemon) throw new Error('Must pass either clientPokemon or serverPokemon');
@@ -80,43 +112,413 @@ ShowdownEnhancedTooltip.showMoveTooltip = function showMoveTooltip(move, isZOrMa
   text += Dex.getTypeIcon(moveType);
   text += ` ${Dex.getCategoryIcon(category)}</h2>`;
 
-  //(stat: StatName, set: PokemonSet, evOverride?: number, natureOverride?: number)
-  //PokemonSet 
-  //pokemon.getStat(atk, pokemon, 84, natureOverride) {
+  console.log(pokemon.side.foe.active[0].name);
+  // Use the getBaseSpecies method
+  let foePokemonBaseSpecies = pokemon.side.foe.active[0].getBaseSpecies();
+  //let baseStats = baseSpecies.baseStats;
+  let foeStats = calculateStats(foePokemonBaseSpecies, pokemon.side.foe.active[0].level);
 
+  text += '<p><small>Base Stats</small></p>';
+  let buf = '';
+  for (const statName of Object.keys(foeStats)) {
+    if (this.battle.gen === 1 && statName === 'spd') continue;
+    let statLabel = this.battle.gen === 1 && statName === 'spa' ? 'spc' : statName;
+    buf += statName === 'atk' ? '<small>' : '<small> / ';
+    buf += '' + BattleText[statLabel].statShortName + '&nbsp;</small>';
+    buf += '' + foeStats[statName];
+  }
+  text += buf;
 
-  // Get the stats of both Pokémon
-  let activePokemonStats = this.calculateModifiedStats(pokemon, serverPokemon);
-  console.log(activePokemonStats);
-  let opponentPokemon = pokemon.side.foe.active[0]; // Assuming single battle, adjust for double/triple battles
-  console.log(opponentPokemon);
-  let opponentServerPokemon = this.battle.myPokemon.find(p => p.name === opponentPokemon.name); // Adjust as needed
-  console.log(opponentServerPokemon);
-  let opponentPokemonStats = this.calculateModifiedStats(opponentPokemon, opponentServerPokemon);
-  console.log(opponentPokemonStats);
+  console.log(pokemon.side.active[0].name);
+  // Use the getBaseSpecies method
+  let activePokemonBaseSpecies = pokemon.side.active[0].getBaseSpecies();
+  //let baseStats = baseSpecies.baseStats;
+  let activeStats = calculateStats(activePokemonBaseSpecies, pokemon.side.active[0].level);
 
-  // Append the stats to the tooltip text
-  text += '<hr style="border: 1px solid black; margin: 5px 0;">';
-  text += '<h3>Active Pokémon Stats</h3>';
-  text += `<p><small>HP:</small> ${activePokemonStats.hp}</p>`;
-  text += `<p><small>Atk:</small> ${activePokemonStats.atk}</p>`;
-  text += `<p><small>Def:</small> ${activePokemonStats.def}</p>`;
-  text += `<p><small>SpA:</small> ${activePokemonStats.spa}</p>`;
-  text += `<p><small>SpD:</small> ${activePokemonStats.spd}</p>`;
-  text += `<p><small>Spe:</small> ${activePokemonStats.spe}</p>`;
+  text += '<p><small>Base Stats</small></p>';
+  buf = '';
+  for (const statName of Object.keys(activeStats)) {
+    if (this.battle.gen === 1 && statName === 'spd') continue;
+    let statLabel = this.battle.gen === 1 && statName === 'spa' ? 'spc' : statName;
+    buf += statName === 'atk' ? '<small>' : '<small> / ';
+    buf += '' + BattleText[statLabel].statShortName + '&nbsp;</small>';
+    buf += '' + activeStats[statName];
+  }
+  text += buf + '<br />';
 
-  text += '<hr style="border: 1px solid black; margin: 5px 0;">';
-  text += '<h3>Opponent Pokémon Stats</h3>';
-  text += `<p><small>HP:</small> ${opponentPokemonStats.hp}</p>`;
-  text += `<p><small>Atk:</small> ${opponentPokemonStats.atk}</p>`;
-  text += `<p><small>Def:</small> ${opponentPokemonStats.def}</p>`;
-  text += `<p><small>SpA:</small> ${opponentPokemonStats.spa}</p>`;
-  text += `<p><small>SpD:</small> ${opponentPokemonStats.spd}</p>`;
-  text += `<p><small>Spe:</small> ${opponentPokemonStats.spe}</p>`;
+  let damageRange = calculateDamage(move, activeStats, foeStats, activePokemonBaseSpecies, foePokemonBaseSpecies, pokemon.side.active[0].level);
+  //turn into percentage
+  damageRange[0] = (damageRange[0] / foeStats.hp * 100).toFixed(1);
+  damageRange[1] = (damageRange[1] / foeStats.hp * 100).toFixed(1);
+  text += damageRange[0] + "% - " + damageRange[1] + "%";
+
 
   return text;
 }
 
+
+const typeEffectivenessChart = {
+  "Normal": {
+    "Normal": 1,
+    "Fire": 1,
+    "Water": 1,
+    "Electric": 1,
+    "Grass": 1,
+    "Ice": 1,
+    "Fighting": 1,
+    "Poison": 1,
+    "Ground": 1,
+    "Flying": 1,
+    "Psychic": 1,
+    "Bug": 1,
+    "Rock": 0.5,
+    "Ghost": 0,
+    "Dragon": 1,
+    "Dark": 1,
+    "Steel": 0.5,
+    "Fairy": 1
+  },
+  "Fire": {
+    "Normal": 1,
+    "Fire": 0.5,
+    "Water": 0.5,
+    "Electric": 1,
+    "Grass": 2,
+    "Ice": 2,
+    "Fighting": 1,
+    "Poison": 1,
+    "Ground": 1,
+    "Flying": 1,
+    "Psychic": 1,
+    "Bug": 2,
+    "Rock": 0.5,
+    "Ghost": 1,
+    "Dragon": 0.5,
+    "Dark": 1,
+    "Steel": 2,
+    "Fairy": 1
+  },
+  "Water": {
+    "Normal": 1,
+    "Fire": 2,
+    "Water": 0.5,
+    "Electric": 1,
+    "Grass": 0.5,
+    "Ice": 1,
+    "Fighting": 1,
+    "Poison": 1,
+    "Ground": 2,
+    "Flying": 1,
+    "Psychic": 1,
+    "Bug": 1,
+    "Rock": 1,
+    "Ghost": 1,
+    "Dragon": 0.5,
+    "Dark": 1,
+    "Steel": 1,
+    "Fairy": 1
+  },
+  "Electric": {
+    "Normal": 1,
+    "Fire": 1,
+    "Water": 2,
+    "Electric": 0.5,
+    "Grass": 0.5,
+    "Ice": 1,
+    "Fighting": 1,
+    "Poison": 1,
+    "Ground": 0,
+    "Flying": 2,
+    "Psychic": 1,
+    "Bug": 1,
+    "Rock": 1,
+    "Ghost": 1,
+    "Dragon": 0.5,
+    "Dark": 1,
+    "Steel": 1,
+    "Fairy": 1
+  },
+  "Grass": {
+    "Normal": 1,
+    "Fire": 0.5,
+    "Water": 2,
+    "Electric": 1,
+    "Grass": 0.5,
+    "Ice": 1,
+    "Fighting": 1,
+    "Poison": 0.5,
+    "Ground": 2,
+    "Flying": 0.5,
+    "Psychic": 1,
+    "Bug": 0.5,
+    "Rock": 2,
+    "Ghost": 1,
+    "Dragon": 0.5,
+    "Dark": 1,
+    "Steel": 0.5,
+    "Fairy": 1
+  },
+  "Ice": {
+    "Normal": 1,
+    "Fire": 0.5,
+    "Water": 0.5,
+    "Electric": 1,
+    "Grass": 2,
+    "Ice": 0.5,
+    "Fighting": 1,
+    "Poison": 1,
+    "Ground": 2,
+    "Flying": 2,
+    "Psychic": 1,
+    "Bug": 1,
+    "Rock": 1,
+    "Ghost": 1,
+    "Dragon": 2,
+    "Dark": 1,
+    "Steel": 0.5,
+    "Fairy": 1
+  },
+  "Fighting": {
+    "Normal": 2,
+    "Fire": 1,
+    "Water": 1,
+    "Electric": 1,
+    "Grass": 1,
+    "Ice": 1,
+    "Fighting": 1,
+    "Poison": 0.5,
+    "Ground": 1,
+    "Flying": 0.5,
+    "Psychic": 0.5,
+    "Bug": 0.5,
+    "Rock": 2,
+    "Ghost": 0,
+    "Dragon": 1,
+    "Dark": 2,
+    "Steel": 2,
+    "Fairy": 0.5
+  },
+  "Poison": {
+    "Normal": 1,
+    "Fire": 1,
+    "Water": 1,
+    "Electric": 1,
+    "Grass": 2,
+    "Ice": 1,
+    "Fighting": 1,
+    "Poison": 0.5,
+    "Ground": 0.5,
+    "Flying": 1,
+    "Psychic": 1,
+    "Bug": 1,
+    "Rock": 0.5,
+    "Ghost": 0.5,
+    "Dragon": 1,
+    "Dark": 1,
+    "Steel": 0,
+    "Fairy": 2
+  },
+  "Ground": {
+    "Normal": 1,
+    "Fire": 2,
+    "Water": 1,
+    "Electric": 2,
+    "Grass": 0.5,
+    "Ice": 1,
+    "Fighting": 1,
+    "Poison": 2,
+    "Ground": 1,
+    "Flying": 0,
+    "Psychic": 1,
+    "Bug": 1,
+    "Rock": 2,
+    "Ghost": 1,
+    "Dragon": 1,
+    "Dark": 1,
+    "Steel": 2,
+    "Fairy": 1
+  },
+  "Flying": {
+    "Normal": 1,
+    "Fire": 1,
+    "Water": 1,
+    "Electric": 0.5,
+    "Grass": 2,
+    "Ice": 1,
+    "Fighting": 2,
+    "Poison": 1,
+    "Ground": 1,
+    "Flying": 1,
+    "Psychic": 1,
+    "Bug": 2,
+    "Rock": 0.5,
+    "Ghost": 1,
+    "Dragon": 1,
+    "Dark": 1,
+    "Steel": 0.5,
+    "Fairy": 1
+  },
+  "Psychic": {
+    "Normal": 1,
+    "Fire": 1,
+    "Water": 1,
+    "Electric": 1,
+    "Grass": 1,
+    "Ice": 1,
+    "Fighting": 2,
+    "Poison": 2,
+    "Ground": 1,
+    "Flying": 1,
+    "Psychic": 0.5,
+    "Bug": 1,
+    "Rock": 1,
+    "Ghost": 1,
+    "Dragon": 1,
+    "Dark": 0,
+    "Steel": 0.5,
+    "Fairy": 1
+  },
+  "Bug": {
+    "Normal": 1,
+    "Fire": 0.5,
+    "Water": 1,
+    "Electric": 1,
+    "Grass": 2,
+    "Ice": 1,
+    "Fighting": 0.5,
+    "Poison": 1,
+    "Ground": 1,
+    "Flying": 0.5,
+    "Psychic": 2,
+    "Bug": 1,
+    "Rock": 1,
+    "Ghost": 0.5,
+    "Dragon": 1,
+    "Dark": 2,
+    "Steel": 0.5,
+    "Fairy": 0.5
+  },
+  "Rock": {
+    "Normal": 1,
+    "Fire": 2,
+    "Water": 1,
+    "Electric": 1,
+    "Grass": 1,
+    "Ice": 2,
+    "Fighting": 0.5,
+    "Poison": 1,
+    "Ground": 0.5,
+    "Flying": 2,
+    "Psychic": 1,
+    "Bug": 2,
+    "Rock": 1,
+    "Ghost": 1,
+    "Dragon": 1,
+    "Dark": 1,
+    "Steel": 0.5,
+    "Fairy": 1
+  },
+  "Ghost": {
+    "Normal": 0,
+    "Fire": 1,
+    "Water": 1,
+    "Electric": 1,
+    "Grass": 1,
+    "Ice": 1,
+    "Fighting": 1,
+    "Poison": 1,
+    "Ground": 1,
+    "Flying": 1,
+    "Psychic": 1,
+    "Bug": 1,
+    "Rock": 1,
+    "Ghost": 2,
+    "Dragon": 1,
+    "Dark": 0.5,
+    "Steel": 1,
+    "Fairy": 1
+  },
+  "Dragon": {
+    "Normal": 1,
+    "Fire": 1,
+    "Water": 1,
+    "Electric": 1,
+    "Grass": 1,
+    "Ice": 1,
+    "Fighting": 1,
+    "Poison": 1,
+    "Ground": 1,
+    "Flying": 1,
+    "Psychic": 1,
+    "Bug": 1,
+    "Rock": 1,
+    "Ghost": 1,
+    "Dragon": 2,
+    "Dark": 1,
+    "Steel": 0.5,
+    "Fairy": 0
+  },
+  "Dark": {
+    "Normal": 1,
+    "Fire": 1,
+    "Water": 1,
+    "Electric": 1,
+    "Grass": 1,
+    "Ice": 1,
+    "Fighting": 0.5,
+    "Poison": 1,
+    "Ground": 1,
+    "Flying": 1,
+    "Psychic": 2,
+    "Bug": 1,
+    "Rock": 1,
+    "Ghost": 2,
+    "Dragon": 1,
+    "Dark": 0.5,
+    "Steel": 1,
+    "Fairy": 0.5
+  },
+  "Steel": {
+    "Normal": 1,
+    "Fire": 0.5,
+    "Water": 0.5,
+    "Electric": 0.5,
+    "Grass": 1,
+    "Ice": 2,
+    "Fighting": 1,
+    "Poison": 1,
+    "Ground": 1,
+    "Flying": 1,
+    "Psychic": 1,
+    "Bug": 1,
+    "Rock": 2,
+    "Ghost": 1,
+    "Dragon": 1,
+    "Dark": 1,
+    "Steel": 0.5,
+    "Fairy": 2
+  },
+  "Fairy": {
+    "Normal": 1,
+    "Fire": 0.5,
+    "Water": 1,
+    "Electric": 1,
+    "Grass": 1,
+    "Ice": 1,
+    "Fighting": 2,
+    "Poison": 0.5,
+    "Ground": 1,
+    "Flying": 1,
+    "Psychic": 1,
+    "Bug": 1,
+    "Rock": 1,
+    "Ghost": 1,
+    "Dragon": 2,
+    "Dark": 2,
+    "Steel": 0.5,
+    "Fairy": 1
+  }
+};
 
 BattleTooltips.prototype.showPokemonTooltip = ShowdownEnhancedTooltip.showPokemonTooltip;
 BattleTooltips.prototype.showMoveTooltip = ShowdownEnhancedTooltip.showMoveTooltip;
